47a29ee44c4185d8289c69668b764691204d1919
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-18 14:33:54 -0800

    ovrride logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022235315) do
+ActiveRecord::Schema.define(version: 20161118214909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
