6b5146cef22fd362fa09a607815529a06512fe41
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-17 10:57:02 -0800

    cli work with glog

--- web/run.go
+++ web/run.go
@@ -1,18 +1,74 @@
 package web
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
+func glogFlagShim(fakeVals map[string]string) {
+	flag.VisitAll(func(fl *flag.Flag) {
+		if val, ok := fakeVals[fl.Name]; ok {
+			fl.Value.Set(val)
+		}
+	})
+}
+
+func glogGangstaShim(c *cli.Context) {
+	_ = flag.CommandLine.Parse([]string{})
+	glogFlagShim(map[string]string{
+		"v":                fmt.Sprint(c.Int("v")),
+		"logtostderr":      fmt.Sprint(c.Bool("logtostderr")),
+		"stderrthreshold":  fmt.Sprint(c.Int("stderrthreshold")),
+		"alsologtostderr":  fmt.Sprint(c.Bool("alsologtostderr")),
+		"vmodule":          c.String("vmodule"),
+		"log_dir":          c.String("log_dir"),
+		"log_backtrace_at": c.String("log_backtrace_at"),
+	})
+}
+
+var glogGangstaFlags = []cli.Flag{
+	cli.IntFlag{
+		Name: "v", Value: 0, Usage: "log level for V logs",
+	},
+	cli.BoolFlag{
+		Name: "logtostderr", Usage: "log to standard error instead of files",
+	},
+	cli.IntFlag{
+		Name:  "stderrthreshold",
+		Usage: "logs at or above this threshold go to stderr",
+	},
+	cli.BoolFlag{
+		Name: "alsologtostderr", Usage: "log to standard error as well as files",
+	},
+	cli.StringFlag{
+		Name:  "vmodule",
+		Usage: "comma-separated list of pattern=N settings for file-filtered logging",
+	},
+	cli.StringFlag{
+		Name: "log_dir", Usage: "If non-empty, write log files in this directory",
+	},
+	cli.StringFlag{
+		Name:  "log_backtrace_at",
+		Usage: "when logging hits line file:N, emit a stack trace",
+		Value: ":0",
+	},
+}
+
 func Run(version string) error {
+	cli.VersionFlag = cli.BoolFlag{
+		Name:  "version",
+		Usage: "print the version",
+	}
 
 	app := cli.NewApp()
 	app.Name = os.Args[0]
 	app.Version = version
 	app.Usage = "CHAMPAK - A complete open source e-commerce solution by go-lang."
 	app.EnableBashCompletion = true
+	app.Flags = append(app.Flags, glogGangstaFlags...)
 	app.Commands = []cli.Command{}
 
 	for _, en := range engines {
@@ -20,5 +76,10 @@ func Run(version string) error {
 		app.Commands = append(app.Commands, cmd...)
 	}
 
+	app.Action = func(c *cli.Context) error {
+		glogGangstaShim(c)
+		cli.ShowAppHelp(c)
+		return nil
+	}
 	return app.Run(os.Args)
 }
