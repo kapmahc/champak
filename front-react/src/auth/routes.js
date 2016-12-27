0a670e6999bb4be6353d30a960d65b34489bc400
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-27 10:09:11 -0800

    auth routes

---
+++ front-react/src/auth/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Route} from 'react-router'
+
+import {SignIn, SignUp, Confirm, Unlock, ForgotPassword, ResetPassword} from './non-sign-in'
+
+const W =(
+  <Route path="users">
+    <Route path="sign-in" component={SignIn}/>
+    <Route path="sign-up" component={SignUp}/>
+    <Route path="confirm" component={Confirm}/>
+    <Route path="unlock" component={Unlock}/>
+    <Route path="forgot-password" component={ForgotPassword}/>
+    <Route path="reset-password/:token" component={ResetPassword}/>
+  </Route>
+)
+
+export default W
--- front-react/src/render.js
+++ front-react/src/render.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import {render} from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
@@ -10,7 +10,7 @@ import './main.css';
 import * as reducers from './reducers'
 
 import App from './App';
-import {SignIn, SignUp, Confirm, Unlock, ForgotPassword, ResetPassword} from './auth/users'
+import Auth from './auth/routes'
 import Home from './components/Home'
 
 let store = createStore(
@@ -27,12 +27,7 @@ render(
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={Home}/>
-        <Route path="users/sign-in" component={SignIn}/>
-        <Route path="users/sign-up" component={SignUp}/>
-        <Route path="users/confirm" component={Confirm}/>
-        <Route path="users/unlock" component={Unlock}/>
-        <Route path="users/forgot-password" component={ForgotPassword}/>
-        <Route path="users/reset-password/:token" component={ResetPassword}/>
+        {Auth}
       </Route>
     </Router>
   </Provider>,
