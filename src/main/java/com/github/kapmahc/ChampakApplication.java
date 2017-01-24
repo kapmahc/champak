117c5aba91977e2874fcdf6d35e9d1b0b9ee3136
Jitang Zheng <jitang.zheng@gmail.com>
2017-01-23 17:07:48 -0800

    create project: https://start.spring.io/

---
+++ src/main/java/com/github/kapmahc/ChampakApplication.java
@@ -0,0 +1,12 @@
+package com.github.kapmahc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampakApplication.class, args);
+	}
+}
